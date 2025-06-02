eff602532c1dccdc78a52a34162610107f42ca13
nozh1k <gnozhkin1@gmail.com>
2025-06-02 14:43:34 +0300

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,12 @@
-module.exports = {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   darkMode: "class",
-  theme: { extend: {} },
+  theme: {
+    extend: {},
+  },
   plugins: [],
 }
